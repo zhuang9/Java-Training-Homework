2a71fde5ea6bb003a3921a96c33afbe39fea6a83
zhuang9 <zhehuang@mtu.edu>
2015-07-26 17:15:52 -0700

    Upload HomeWork0716_Question1

---
+++ HomeWork0716/Question1/BufferedReaderWriter.java
@@ -0,0 +1,66 @@
+package Question1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BufferedReaderWriter {
+
+	public static void main(String[] args) {
+		BufferedReaderWriter brw = new BufferedReaderWriter();
+		brw.BufferedWriterExample();// Create a new file if the file not exists
+									// and write new content in it
+		brw.BufferedReaderExample(null);// Read the content from new file
+	}
+
+	void BufferedWriterExample() {
+
+		try {
+			String content = "This is the content to write into file";
+
+			File file = new File("/Users/zhehuang/Desktop/New_File.txt");
+
+			// if file doesn't exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content);
+			bw.close();
+
+			System.out.println("Done");
+		}
+
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	void BufferedReaderExample(BufferedReader br) {
+		try {
+
+			String sCurrentLine;
+
+			br = new BufferedReader(new FileReader("/Users/zhehuang/Desktop/New_File.txt"));
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
